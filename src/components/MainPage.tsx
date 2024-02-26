9eb68b4fcccee7ee2aaaf98620557c5f01e52f30
NickPodguzov993 <verytallman993@gmail.com>
2024-02-26 17:42:53 +0300

    commit init

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -11,12 +11,13 @@ const MainPage = () => {
     return (
         <div>
             <div>
-                <h1 className='p-8'>Оказываем <br/> <span className={style.span}>спектр услуг </span>
+                <h1 className='p-8 text-[44px] leading-[46px] m-auto mt-[52px]'>Оказываем <br/> <span className={style.span}>спектр услуг </span>
                     в<br/> сфере<br/> сопровождения<br/> деятельности<br/> <span className={style.span}> it-компаний</span> </h1>
             </div>
             <SliderAbility/>
             <Services/>
             <Goals/>
+
         </div>
 
     );
